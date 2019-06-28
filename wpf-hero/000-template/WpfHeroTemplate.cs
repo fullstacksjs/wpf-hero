ce12d753ab23257967f92bdd572c1a8dd22aefdb
Pavel Erokhin <MairwunNx@gmail.com>
2019-06-28 23:23:45 +0300

    ✒️ Project type changed from application to CustomControl.
    Also uploaded fonts Rajdhani all styles.

---
+++ wpf-hero/000-template/WpfHeroTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _000_template
+{
+    public class WpfHeroTemplate : Control
+    {
+        private readonly Type TypeofThis = typeof(WpfHeroTemplate);
+
+        static WpfHeroTemplate()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(WpfHeroTemplate),
+                new FrameworkPropertyMetadata(typeof(WpfHeroTemplate))
+            );
+        }
+    }
+}
