d4fe50da44d1862d7154ef2c8bbf1e5763f1f95d
Pavel Erokhin <MairwunNx@gmail.com>
2019-06-29 19:03:06 +0300

    ✨ Added base layout features, background and card.

--- wpf-hero/000-template/WpfHeroTemplate.cs
+++ wpf-hero/000-template/WpfHeroTemplate.cs
@@ -6,14 +6,56 @@ namespace _000_template
 {
     public class WpfHeroTemplate : Control
     {
-        private readonly Type TypeofThis = typeof(WpfHeroTemplate);
+        private static readonly Type TypeofThis = typeof(WpfHeroTemplate);
 
         static WpfHeroTemplate()
         {
             DefaultStyleKeyProperty.OverrideMetadata(
                 typeof(WpfHeroTemplate),
-                new FrameworkPropertyMetadata(typeof(WpfHeroTemplate))
+                new FrameworkPropertyMetadata(TypeofThis)
             );
         }
+
+        /// <summary>
+        /// Set or get the width of the base container in
+        /// which the content resides.
+        /// </summary>
+        public double ContainerWidth
+        {
+            get => (double)GetValue(ContainerWidthProperty);
+            set => SetValue(ContainerWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ContainerWidth"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ContainerWidthProperty =
+            DependencyProperty.Register(
+                nameof(ContainerWidth),
+                typeof(double),
+                TypeofThis,
+                new PropertyMetadata(400.0D)
+            );
+
+        /// <summary>
+        /// Set or get the height of the base container in
+        /// which the content resides.
+        /// </summary>
+        public double ContainerHeight
+        {
+            get => (double)GetValue(ContainerHeightProperty);
+            set => SetValue(ContainerHeightProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ContainerHeight"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ContainerHeightProperty =
+            DependencyProperty.Register(
+                nameof(ContainerHeight),
+                typeof(double),
+                TypeofThis,
+                new PropertyMetadata(400.0D)
+            );
     }
 }
--- wpf-hero/001-searchbar/MainWindow.xaml.cs
+++ wpf-hero/001-searchbar/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace _001_searchbar
 {
@@ -23,6 +10,11 @@ namespace _001_searchbar
         public MainWindow()
         {
             InitializeComponent();
+            Title = $"WPF Hero - Searchbar D: (Height: {Height}, Width: {Width})";
+            SizeChanged += (s, e) =>
+            {
+                Title = $"WPF Hero - Searchbar D: (Height: {Height}, Width: {Width})";
+            };
         }
     }
 }
