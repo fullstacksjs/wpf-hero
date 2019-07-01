9bcab8729d335f167c1bb21af6a7daec60d5588a
Pavel Erokhin <MairwunNx@gmail.com>
2019-07-01 16:29:55 +0300

    🎉 Added \ Created 000-template-demo.

---
+++ wpf-hero/000-template-demo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace _000_template_demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ wpf-hero/000-template-demo/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _000_template_demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
