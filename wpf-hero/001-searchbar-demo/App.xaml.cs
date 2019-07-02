45f3d0342397a567d8b58ab5359a0132c5292d83
Pavel Erokhin <MairwunNx@gmail.com>
2019-07-02 13:16:28 +0300

    ✒ Created 001-searchbar project and 001-searchbar-demo project.

--- wpf-hero/001-searchbar-demo/App.xaml.cs
+++ wpf-hero/001-searchbar-demo/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace _001_searchbar
+namespace _001_searchbar_demo
 {
     /// <summary>
     /// Interaction logic for App.xaml
